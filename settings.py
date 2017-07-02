8054e8ea0fc08cb07ba1d4096543743109fb6d18
SanSanch5 <a.a.pakhomoff@gmail.com>
2017-07-02 21:59:31 +0300

    try to use file based db instaed of in-memory one

--- settings.py
+++ settings.py
@@ -1,3 +1,10 @@
-DEBUG_MODE = True
+import os
+
+DEBUG_MODE = False
 PORT = 5001
-DB_URI = 'sqlite:///:memory:'
+
+BACKEND_PATH = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BACKEND_PATH, 'sessions.db')
+DB_URI = 'sqlite:////' + DB_PATH
+
+# DB_URI = 'sqlite:///:memory:'
